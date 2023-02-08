846a1c63d3d2bd7cea335850290fcb8ab0617a6b
Farrukh Makhkamov <makhkamovfar@gmail.com>
2023-02-08 17:21:06 +0500

    third commit

--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -31,6 +31,7 @@ export default function WeatherInfo(props) {
          <div className="City">{props.userInput}</div>
          <div className="Preview">
               <div className="temp">
+              {is_hot?<img style={temp_value_style} src={hot}/>:<img style={temp_value_style} src={cold}/>}
                 <p>{data.temp_c} </p>
                
                 <img
@@ -38,12 +39,12 @@ export default function WeatherInfo(props) {
                     height: 15,
                     width: 15,
                     marginBottom: 5,
-                    marginLeft: 5,
+                    marginRight: 5,
                   }}
                   src={celcius}
                 />
                 <img alt="#" src={data.condition.icon}></img>
-                {is_hot?<img style={temp_value_style} src={hot}/>:<img style={temp_value_style} src={cold}/>}
+                
               </div>
               <div className="temp">
                 <img src={wind} />
