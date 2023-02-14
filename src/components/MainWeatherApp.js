1bf8f6044efe2ee7a8db321d027c486c00ea115c
Farrukh Makhkamov <makhkamovfar@gmail.com>
2023-02-14 19:08:41 +0500

    commit 8

--- src/components/MainWeatherApp.js
+++ src/components/MainWeatherApp.js
@@ -19,53 +19,61 @@ export default class MainWeatherApp extends Component {
     };
   }
 
-  btnClicked = (e) => {
-    e.preventDefault();
 
-    if (this.state.userInput !== "") {
-      this.setState({ isLoading: true });
-      this.setState({networkError:false})
-      this.setState((state) => ({
-        city: state.userInput,
-      }));
-      axios
-        .get(
-          `${this.apiURl}forecast.json?key=${this.apiKey}&q=${this.state.userInput}&days=7&aqi=no`
-        )
-        .then((response) => {
-          this.setState({
-            networkError: false,
-          });
-          this.setState({
-            apiCurrentData: response.data.forecast.forecastday.map((item) => {
-              return item.day;
-            }),
-          });
-          console.log(this.state.apiCurrentData);
-
-          this.setState({ isLoading: false });
-        })
-        .catch((error) => {
-          console.log(error);
-          this.setState({
-            networkError: true,
-          });
-        });
-    } else {
-      alert("Ничего не введено");
+ callApi=(value,setSessionstorage)=>{
+  axios
+  .get(
+    `${this.apiURl}forecast.json?key=${this.apiKey}&q=${value}&days=7&aqi=no`
+  )
+  .then((response) => {
+    this.setState({
+      networkError: false,
+    });
+    this.setState({
+      apiCurrentData: response.data.forecast.forecastday.map((item) => {
+        return item.day;
+      }),
+    });
+    if(setSessionstorage===true){
+      sessionStorage.setItem(`userInput`, value);
     }
-  };
 
- componentDidMount(){
-
-    
+    this.setState({ isLoading: false });
+  })
+  .catch((error) => {
+    console.log(error);
+    this.setState({
+      networkError: true,
+    });
+  });
  }
 
 
 
 
+  btnClicked = (e) => {
+    e.preventDefault();
 
+    if (this.state.userInput !== "") {
+      this.setState({ isLoading: true });
+      this.setState({ networkError: false });
+      this.setState((state) => ({
+        city: state.userInput,
+      }));
 
+      this.callApi(this.state.userInput,true)
+      
+    } else {
+      alert("Ничего не введено");
+    }
+  };
+
+  componentDidMount() {
+    if (sessionStorage.getItem("userInput")) {
+     this.callApi(sessionStorage.getItem('userInput'),false)
+     this.setState({userInput:sessionStorage.getItem('userInput')})
+    }
+  }
 
   handleChange = (e) => {
     this.setState({
@@ -74,14 +82,14 @@ export default class MainWeatherApp extends Component {
   };
 
   render() {
-   let date=new Date()
+    let date = new Date();
     return (
       <>
-        
-        
-        {this.state.networkError? <div className="GuidenceInfo">Возникла ошибка соединения. Возможно неверно указано название города</div>:
-         
-         this.state.isLoading ? (
+        {this.state.networkError ? (
+          <div className="GuidenceInfo">
+            Возникла ошибка соединения. Возможно неверно указано название города
+          </div>
+        ) : this.state.isLoading ? (
           <div className="lds-roller">
             <div></div>
             <div></div>
@@ -92,25 +100,25 @@ export default class MainWeatherApp extends Component {
             <div></div>
             <div></div>
           </div>
-        ) : 
-        this.state.apiCurrentData? 
-        (
+        ) : this.state.apiCurrentData ? (
           this.state.apiCurrentData.map((item, index) => {
             return (
-              <WeatherInfo day={date.getDate()+index} key={index} apiData={item} city={this.state.city} />
-                );
+              <WeatherInfo
+                day={date.getDate() + index}
+                key={index}
+                apiData={item}
+                city={this.state.city}
+              />
+            );
           })
-        ): 
-        <div className="GuidenceInfo">Введите название города и узнайте прогноз погоды на неделю</div>
-        }
-        
-         
-        
-        
-        
+        ) : (
+          <div className="GuidenceInfo">
+            Введите название города и узнайте прогноз погоды на неделю
+          </div>
+        )}
 
         <form onSubmit={this.btnClicked} className="mainForm">
-          <input onChange={this.handleChange} />
+          <input value={this.state.userInput} onChange={this.handleChange} />
           <button type="submit">Узнать</button>
         </form>
       </>
--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -1,44 +1,55 @@
 import React from "react";
 import celcius from "../static/celcius.png";
-import wind from '../static/wind.svg'
-import raindrop from '../static/raindrop.svg'
-
+import wind from "../static/wind.svg";
+import raindrop from "../static/raindrop.svg";
 
 export default function WeatherInfo(props) {
   const data = props.apiData;
-  let date=new Date()
-  
-  
+  let date = new Date();
 
-  const monthes=[
-    'Янв',"Фев","Март","Апр","Май","Июнь","Июль","Авг","Сен","Окт","Нояб","Дек"
-  ]
- 
-    return (
-      
-        
-          
-          <>
-          <div className="weatherCard">
-            <div className="weatherCardImg">
-                <img alt="#" src={data.condition.icon}/>
-              </div>
-              <div className="weatherCardSubinfo">
-                <div className="tempDate">
-                <p>{data.avgtemp_c}<img src={celcius}/></p>
-                <p>{props.day} {monthes[date.getMonth()]}</p>
-                </div>
-                <div className="windHum">
-                  <span><img src={wind}/><p>{data.maxwind_kph} км/ч</p></span>
-                  <span><img src={raindrop}/><p>{data.avghumidity}%</p></span>
-                </div>
-              </div> 
+  const monthes = [
+    "Янв",
+    "Фев",
+    "Март",
+    "Апр",
+    "Май",
+    "Июнь",
+    "Июль",
+    "Авг",
+    "Сен",
+    "Окт",
+    "Нояб",
+    "Дек",
+  ];
 
-
-             
+  return (
+    <>
+      <div className="weatherCard">
+        <div className="weatherCardImg">
+          <img alt="#" src={data.condition.icon} />
+        </div>
+        <div className="weatherCardSubinfo">
+          <div className="tempDate">
+            <p>
+              {data.avgtemp_c}
+              <img src={celcius} />
+            </p>
+            <p>
+              {props.day} {monthes[date.getMonth()]}
+            </p>
           </div>
-          </>
-      
-    );
-  }
-
+          <div className="windHum">
+            <span>
+              <img src={wind} />
+              <p>{data.maxwind_kph} км/ч</p>
+            </span>
+            <span>
+              <img src={raindrop} />
+              <p>{data.avghumidity}%</p>
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
